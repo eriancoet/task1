2c77f5ffbb64df8c5e774b3456f6af1a7a237e83
Rian Coetzee <eriancoet@gmail.com>
2023-09-08 13:28:44 +0200

    format complete

--- public/contacts.js
+++ public/contacts.js
@@ -1,52 +1,44 @@
-// contacts.js
 document.addEventListener("DOMContentLoaded", () => {
-    const contactList = document.getElementById("contact-list");
+    const contactGrid = document.getElementById("contact-grid").querySelector("tbody");
 
-    // Fetch contacts from the Pipedrive API and populate the list
+    // Fetch contacts from the Pipedrive API and populate the grid
     fetch("contacts")
         .then(response => response.json())
         .then(data => {
             // Check if data is an array of contacts
             if (Array.isArray(data)) {
                 data.forEach(contact => {
-                    // Create a list item for each contact
-                    const listItem = document.createElement("li");
+                    // Create a table row for each contact
+                    const row = document.createElement("tr");
 
                     // Access contact properties such as name, email, phone, etc.
                     const firstName = contact.first_name || "N/A";
                     const lastName = contact.last_name || "N/A";
                     const email = contact.primary_email || "N/A";
                     const phone = contact.phone.value || "N/A";
-                    const companyID = contact.company_id || "N/A"
-                    const company = contact.org_id.name || "N/A"
-                    const address = contact.org_id.address || "N/A"
-                    const followers = contact.followers_count || "N/A"
-                    const deals = contact.closed_deals_count || "N/A"
-                    const joined = contact.add_time || "N/A"
+                    const companyID = contact.company_id || "N/A";
+                    const company = contact.org_id.name || "N/A";
+                    const address = contact.org_id.address || "N/A";
+                    const followers = contact.followers_count || "N/A";
+                    const deals = contact.closed_deals_count || "N/A";
+                    const joined = contact.add_time || "N/A";
 
+                    // Populate the table row with contact information
+                    row.innerHTML = `
+                        <td>${firstName}</td>
+                        <td>${lastName}</td>
+                        <td>${email}</td>
+                        <td>${phone}</td>
+                        <td>${companyID}</td>
+                        <td>${company}</td>
+                        <td>${address}</td>
+                        <td>${followers}</td>
+                        <td>${deals}</td>
+                        <td>${joined}</td>
+                    `;
 
-
-                    console.log(data)
-
-                    // Populate the list item with contact information
-                    listItem.textContent = 
-                    `
-                     Name: ${firstName}, 
-                     Surname: ${lastName}, 
-                     Email: ${email}, 
-                     Phone: ${phone},
-                     companyID: ${companyID},
-                     Company: ${company}, 
-                     Address: ${address},
-                     Followers: ${followers},
-                     Closed Deals: ${deals}
-                     Joined: ${joined}
-                     `;
-
-;
-
-                    // Append the list item to the contact list
-                    contactList.appendChild(listItem);
+                    // Append the table row to the contact grid
+                    contactGrid.appendChild(row);
                 });
             } else {
                 // Handle the case where data is not an array of contacts
